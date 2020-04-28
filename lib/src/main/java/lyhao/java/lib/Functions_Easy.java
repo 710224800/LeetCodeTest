3367a24682e777de9d64d8862985a91d898ef4e0
luyanhao <luyanhao@xiaomi.com>
2020-04-28 16:49:23 +0800

    合并两个有序链表

--- lib/src/main/java/lyhao/java/lib/Functions_Easy.java
+++ lib/src/main/java/lyhao/java/lib/Functions_Easy.java
@@ -23,6 +23,36 @@ public class Functions_Easy {
     }
 
     /**
+     * 合并两个有序链表
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode cur = null;
+        if(l1 != null && (l2 == null || l1.val < l2.val)){
+            head = l1;
+            l1 = l1.next;
+        } else if(l2 != null){
+            head = l2;
+            l2 = l2.next;
+        }
+        cur = head;
+        while(cur != null && (l1 != null || l2 != null)){
+            if(l1 != null && (l2 == null || l1.val < l2.val)){
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    /**
      * 输出 杨辉三角 前numRows行
      * @param numRows
      * @return
