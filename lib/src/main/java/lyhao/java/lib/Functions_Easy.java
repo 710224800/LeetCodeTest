b9229fedd29d277b53c14301ce62b604ebdec0c7
luyanhao <luyanhao@xiaomi.com>
2020-05-20 17:36:49 +0800

    移动0

--- lib/src/main/java/lyhao/java/lib/Functions_Easy.java
+++ lib/src/main/java/lyhao/java/lib/Functions_Easy.java
@@ -23,6 +23,33 @@ public class Functions_Easy {
     }
 
     /**
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * 示例:
+     * 输入: [0,1,0,3,12]
+     * 输出: [1,3,12,0,0]
+     * https://leetcode-cn.com/problems/move-zeroes/solution/dong-hua-yan-shi-283yi-dong-ling-by-wang_ni_ma/
+     * @param nums
+     */
+    public static void moveZeroes(int[] nums) {
+        if(nums==null) {
+            return;
+        }
+        //两个指针i和j
+        int j = 0;
+        for(int i=0;i<nums.length;i++) {
+            //当前元素!=0，就把其交换到左边，等于0的交换到右边
+            if(nums[i]!=0) {
+                if(i > j) { // i == j 时不需要交换
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                }
+                j++;
+            }
+        }
+    }
+
+    /**
      * 合并两个有序链表
      * @param l1
      * @param l2
